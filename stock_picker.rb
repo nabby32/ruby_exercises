4ce53ac7f847139f6bd35c83896bb5b85a5aa3d4
Liam Brenner <franzymope@gmail.com>
2017-05-15 19:40:36 -0400

    Wrote a simple program to evaluate best days to buy and sell stock based on array of stock value.

---
+++ stock_picker.rb
@@ -0,0 +1,27 @@
+def stock_picker(day_array)
+	answer = []
+	profit = 0
+	
+	day_array.each do |day|
+		#Get place of day in the array.
+		place = day_array.index(day)
+		#Check numbers after the day in question for smaller values.
+		while place < day_array.length
+			#If smaller values result in larger profit, update max profit value and the days to achieve it.
+			if (day - day_array[place]) > profit
+				profit = day - day_array[place]
+				answer.clear
+				answer.push(day_array.index(day))
+				answer.push(place)
+			end
+			#Advance to the next day.
+			place += 1
+		end
+	end
+	if profit > 0
+		return "Your best bet was to buy on day #{answer[0]} and sell on day #{answer[1]} for a profit of $#{profit} per share."
+	elsif profit == 0
+		return "Hopefully you didn't buy this, because it only went down--no profit to be made."
+	end
+end
+
