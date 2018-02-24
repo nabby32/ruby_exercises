36ffda94faec557b33433c4e3c9dd1498d62832f
Liam Brenner <franzymope@gmail.com>
2018-02-23 22:49:08 -0500

    First update in a long while. Advanced to player order, player code selection.

--- codebreaker.rb
+++ codebreaker.rb
@@ -1,112 +1,100 @@
 
-
+#Note:
+#Next step is code for arrays to check against eachother and return match results.
 class Codebreaker
 	@@turns = 0
 	@@games = 0
-	#Initialize game based on 1 or 2 players.
 	def initialize
 		@player1 = Player.new
-		@player2 = Player.new
-		@opponent = @player2
+		@computer = Computer.new
 		@board = Board.new
 		welcome
 	end
 
 	#Welcome both players.
 	def welcome
-		puts "\nWelcome message\n"
+		puts "\nWelcome to Codebreaker, #{@player1.name}. In this game,
+		you will have 12 guesses to break the computer's
+		code, which is comprised of four random numbers
+		from 1 to 6.\n"
 		@board.display
-		rounds
-		if first_up
-			puts "\nGreat, #{@rounds_num} round(s). #{@opponent.name}, you will guess #{@player1.name}'s code first."
+		puts "\nFirst, let's see if you or the computer will try code breaking first.\n
+		Determining order...\n"
+		order_creator
+	end
+
+	#Establish first coder/guesser.
+	def order_creator
+		num = (rand(100)/100).to_f
+		if num >= 0.5
+			@player1.order = 2
+			@computer.order = 1
+			puts "\nIt looks like the computer will code first, so you will guess.\n"
+			computer_code
 		else
-			puts "\nGreat, #{@rounds_num} round(s). #{@player1.name}, you will guess #{@opponent.name}'s code first."
-		end
-		puts "\n"
-		pick_code
-	end
-
-	#Have player select the number of rounds to play.
-	def rounds
-		puts "\nPlease enter the number of rounds you'd like to play."
-		@rounds_num = gets.chomp
-		unless @rounds_num.to_i
-			puts "Please enter a digit.\n"
-			rounds
-		end
-		@rounds_num = @rounds_num.to_i
-	end
-
-	#Randomly select the first person coding and first guessing.
-	def first_up
-		if rand(2) > 0
-			@code_breaker = @opponent
-			@coder = @player1
-			true
-		else
-			@code_breaker = @player1
-			@coder = @opponent
-			false
-		end
-	end
-	#Coder picks code.
-	def pick_code
-		puts "OK, #{@coder.name}, please enter your code as four letters, with each letter representing the first letter of your chosen color (ex. gyrp)."
-		code = gets.chomp.downcase.split("")
-		@coder.code << code
-		puts "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nGot it! No peaking, #{@code_breaker.name}!"
-		unless checker
-			puts "Oops, that's not a valid input. Rembmer to just enter the first letter of four of the available colors: purple, red, green, blue, yellow, and orange."
-			pick_code
-		end
-		puts "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nGot it! No peaking, #{@code_breaker.name}!"
-		code_breaking
-	end
-
-	def checker
-		if @coder.code[0].split(/\s/).length != 4
-			false
-		else
-			true
+			@player1.order = 1
+			@computer.order = 2
+			puts "\n#{@player1.name}, you'll code first, and the computer will guess.\n"
+			player_code
 		end
 	end
 
-	def player_switch(current)
-		if current == @player1
-			@coder = @player2
-			@codebreaker = @opponent
-		elsif current == @opponent
-			@coder = @player1
-			@codebreaker = @opponent
+	#Randomly generate the computer's color code.
+	def computer_code
+		4.times do
+			@computer.code << rand(6)
 		end
-		#Return to game 
 	end
 
+	#Player creates code
+	def player_code
+		puts "\n#{@player1.name}, please enter your four digit code."
+		@player1.code = gets.chomp.scan(/\d/).to_a
+		computer_guess
+	end
 
+	#Player guesses code
+	def player_guess
+		puts "The computer has created its code. Please enter guess number #{@player1.guess_count}."
+		@player1.guesses << gets.chomp.scan(/\d/).to_a
+		@player1.guess_count += 1
+		#Code to compare guesses against computer code.
+
+	end
 
 end
 
 class Player
-	attr_reader :name, :code, :guesses
-	@@players = 0
+	#Establish player with name, their secret code, and their guesses.
+	attr_reader :name
+	attr_accessor :code, :guesses, :guess_count, :order
+	@@players = 1
 	def initialize
-		if @@players >= 2
-			@@players = 0
-			@@players += 1
-		elsif @@players < 2
-			@@players += 1
-		end
 		puts "\nPlayer #{@@players}, please enter your name."
 		@name = gets.chomp.downcase.capitalize
 		@code = []
 		@guesses = []
+		@guess_count = 0
+		@order = 0
+	end
+end
+
+class Computer
+	#Establish computer with secret code, and guesses.
+	attr_accessor :code, :guesses, :guess_count, :order
+	def initialize
+		@code = []
+		@guesses = []
+		@guess_count = 0
+		@order = 0
 	end
 end
 
 class Board
+	#Establish a blank board showing layout only.
 	def initialize
 		@board = []
-		40.times do
+		48.times do
 			@board << " + "
 		end
 	end
@@ -121,8 +109,15 @@ class Board
 		puts "| " + @board[24..27].join(" | ") + " |" + "\n"
 		puts "| " + @board[28..31].join(" | ") + " |" + "\n"
 		puts "| " + @board[32..35].join(" | ") + " |" + "\n"
-		puts "| " + @board[36..39].join(" | ") + " |" + "\n\n"
+		puts "| " + @board[36..39].join(" | ") + " |" + "\n"
+		puts "| " + @board[40..43].join(" | ") + " |" + "\n"
+		puts "| " + @board[44..47].join(" | ") + " |" + "\n\n"
 	end
 end
 
+
+#Initalize game: create a new player, and a new board.
+#Welcome player to the game.
+#Establish coding order; player 1 or computer first.
+#Establish code, from computer or player.
 
