df1ee2147ce406534950fbaa182cec5eb4904baa
Liam Brenner <franzymope@gmail.com>
2018-02-26 17:48:41 -0500

    Second try at codebreaker from the ground up.

---
+++ codebreaker2.rb
@@ -0,0 +1,26 @@
+#Welcome to Codebreaker
+#Ask player name
+#Determine if player will code first or guess first
+#Coder creates code
+#Guesses inupts first guess
+#Compare guess with code
+#Inform guesser of success/failure
+#Repeat until turns expire or guess is fully successful
+
+class Codebreaker
+
+class Player
+	attr_reader
+	attr_accessor :guess_number :guesses :code :name :order
+	def initiate
+		
+	end	
+end
+
+class AI
+end
+
+class Board
+end
+
+end
